aeae7ff247102d03451664cfcb0af72944df0d90
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-06-05 00:16:18 +0530

    init project

---
+++ extension/src/App.tsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-[350px] min-h-screen bg-zinc-950 text-white p-4">
+      <h1 className="text-2xl font-bold">
+        ThinkDSA 🚀
+      </h1>
+
+      <p className="text-zinc-400 mt-2">
+        Learn DSA by thinking, not memorizing.
+      </p>
+
+      <div className="mt-6 bg-zinc-900 p-4 rounded-xl">
+        <h2 className="font-semibold">
+          Problem Detected
+        </h2>
+
+        <p className="text-zinc-300 mt-2">
+          56. Merge Intervals
+        </p>
+      </div>
+
+      <div className="mt-6 flex flex-col gap-3">
+        <button className="bg-blue-600 py-2 rounded-lg">
+          Get Hint
+        </button>
+
+        <button className="bg-zinc-800 py-2 rounded-lg">
+          Find Pattern
+        </button>
+
+        <button className="bg-zinc-800 py-2 rounded-lg">
+          Explain Concept
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
