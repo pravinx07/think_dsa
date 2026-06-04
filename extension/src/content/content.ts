117a07091d034cc282c32ab5cb98e5bd1d914c9b
Pravin Shegamwar <shegamwarpravin07@gmail.com>
2026-06-05 00:48:21 +0530

    detect the problem title

--- extension/src/content/content.ts
+++ extension/src/content/content.ts
@@ -1,1 +1,17 @@
-console.log("ThinkDSA content script loaded succesfully");
+function getProblemTitle() {
+  const titleElement = document.querySelector(
+    ".text-title-large"
+  );
+
+  const title = titleElement?.textContent;
+
+  if (title) {
+    chrome.storage.local.set({
+      problemTitle: title,
+    });
+
+    console.log("Saved title:", title);
+  }
+}
+
+getProblemTitle();
